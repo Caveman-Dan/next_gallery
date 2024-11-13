947c97cfc4e356ebee5f1844a09a3b408cc5bdfa
Dan <dan@waxworlds.org>
2024-11-13 16:20:17 +0000

    removed JS for logo switch, now done with css

--- src/app/ui/home/Logo.tsx
+++ src/app/ui/home/Logo.tsx
@@ -3,33 +3,32 @@
 import Link from "next/link";
 import Image from "next/image";
 
-import useWindowSize from "@/hooks/useWindowSize";
 import LogoWithName from "@/assets/logoWithName.svg";
 import LogoIcon from "@/assets/logo.svg";
 
 import styles from "./logo.module.scss";
-import breakpoints from "@/style/breakpoints.json";
 
-const dispenseLogo = (width) =>
-  width >= breakpoints.md ? LogoWithName : LogoIcon;
-
-const Logo = () => {
-  const windowSize = useWindowSize();
-
-  return (
-    <div className={styles.root}>
-      <Link href="/" className={styles.link}>
-        <Image
-          className={styles.image}
-          src={dispenseLogo(windowSize.width)}
-          alt="Next Gallery Logo"
-          fill
-          // width="48"
-          // height="48"
-        />
-      </Link>
-    </div>
-  );
-};
+const Logo = () => (
+  <div className={styles.root}>
+    <Link href="/" className={styles.link}>
+      <Image
+        className={styles.imageSm}
+        src={LogoIcon}
+        alt="Next Gallery Logo"
+        fill
+        // width="48"
+        // height="48"
+      />
+      <Image
+        className={styles.imageMd}
+        src={LogoWithName}
+        alt="Next Gallery Logo"
+        fill
+        // width="48"
+        // height="48"
+      />
+    </Link>
+  </div>
+);
 
 export default Logo;

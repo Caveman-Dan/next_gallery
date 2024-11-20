b2c39856be542d7cba5eee1c10fdb8f827f5e042
Dan <dan@waxworlds.org>
2024-11-20 13:24:34 +0000

    SASS now automatically loads global_imports.scss

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,8 @@ import jsonImporter from "node-sass-json-importer";
 const nextConfig: NextConfig = {
   sassOptions: {
     importer: jsonImporter(),
+    includePaths: ["./src/app/style"],
+    prependData: `@import "global_imports";`,
   },
 };
 

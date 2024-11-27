b6c89a1c415f930acf1e928c08118c9392bf28a0
Dan <dan@waxworlds.org>
2024-11-27 12:05:00 +0000

    Ripple effect now reusable, updated Button component

--- src/app/ui/components/Button/Button.tsx
+++ src/app/ui/components/Button/Button.tsx
@@ -2,41 +2,17 @@
 
 import React from "react";
 
+import handleRipple from "@/ui/components/Ripple/Ripple";
+
 import styles from "./Button.module.scss";
 
 type ButtonProps = {
   children: React.ReactNode;
-  callback?: (event: React.MouseEvent) => void;
 };
 
-const createRipple = (event: React.MouseEvent<HTMLButtonElement>) => {
-  const button = event.currentTarget;
-  const circle = document.createElement("span");
-  const diameter = Math.max(button.clientWidth, button.clientHeight);
-  const radius = diameter / 2;
-
-  circle.style.width = circle.style.height = `${diameter}px`;
-  circle.style.left = `${event.clientX - (button.offsetLeft + radius)}px`;
-  circle.style.top = `${event.clientY - (button.offsetTop + radius)}px`;
-  circle.classList.add(styles.ripple);
-
-  const ripple = button.getElementsByClassName(styles.ripple)[0];
-
-  if (ripple) {
-    ripple.remove();
-  }
-
-  button.appendChild(circle);
-};
-
-const Button: React.FC<ButtonProps> = ({ children, callback }) => {
-  const handleClick = (event: React.MouseEvent<HTMLButtonElement>) => {
-    createRipple(event);
-    if (callback) callback(event);
-  };
-
+const Button: React.FC<ButtonProps> = ({ children }) => {
   return (
-    <button className={styles.root} onClick={handleClick}>
+    <button className={styles.root} onClick={handleRipple}>
       {children}
     </button>
   );
---
+++ src/app/ui/components/Ripple/Ripple.tsx
@@ -0,0 +1,25 @@
+import styles from "./Ripple.module.scss";
+
+const handleRipple = (event: React.MouseEvent<HTMLButtonElement>) => {
+  const element = event.currentTarget;
+  element?.classList.add(styles.root);
+
+  const circle = document.createElement("span");
+  const diameter = Math.max(element.clientWidth, element.clientHeight);
+  const radius = diameter / 2;
+
+  circle.style.width = circle.style.height = `${diameter}px`;
+  circle.style.left = `${event.clientX - (element.offsetLeft + radius)}px`;
+  circle.style.top = `${event.clientY - (element.offsetTop + radius)}px`;
+  circle.classList.add(styles.ripple);
+
+  const ripple = document.getElementsByClassName(styles.ripple)[0];
+
+  if (ripple) {
+    ripple.remove();
+  }
+
+  element.appendChild(circle);
+};
+
+export default handleRipple;

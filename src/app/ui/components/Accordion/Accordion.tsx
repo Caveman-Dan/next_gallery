68dd9e253462a3e20da0d6c38d6cb1d3912a66c4
Dan <dan@waxworlds.org>
2025-01-09 16:36:07 +0000

    Accordion vastly improved, still think I can remove some of the props tho (WIP)

--- src/app/ui/components/Accordion/Accordion.tsx
+++ src/app/ui/components/Accordion/Accordion.tsx
@@ -12,9 +12,6 @@ import { menuItems as springsConfig } from "@/style/springsConfig";
 const ExpandingLayer = ({
   entry, // The folder's data object
   onSelect, // Callback for onSelect
-  stateDispatchArray, // Array of stateDispatch functions for each closure
-  setStateDispatchArray,
-  parentId, // To select parent when closing a layer
   parentIsOpen, // To determine when to hide items and collapse an open list
   siblingIsSelected, // To hide an item only when a sibling has been selected
   setSiblingIsSelected,
@@ -24,8 +21,7 @@ const ExpandingLayer = ({
   parentsListHeight, // When closing a layer the height must be set to that of its parent
   focusedItem,
   setFocusedItem,
-  parentEntry = null,
-  depth, // To determine which layers to close
+  parentEntry,
   root = false, // To prevent root items from being hidden in a collapsed list
 }) => {
   const [sectionOpen, setSectionOpen] = useState(false);
@@ -40,23 +36,25 @@ const ExpandingLayer = ({
 
   useLayoutEffect(() => {
     if (!renderChildren) return;
-    if (stateDispatchArray[entry.id]) return;
-
-    setStateDispatchArray({
-      ...stateDispatchArray,
-      [entry.id]: { expand: setSectionOpen, select: setIsSelected, depth },
-    });
-  }, [depth, entry.id, renderChildren, setStateDispatchArray, stateDispatchArray]);
-
-  useLayoutEffect(() => {
-    if (!renderChildren) return;
     if (!entry) return;
     if (!focusedItem) return;
 
     if (entry.id === focusedItem.id) {
+      // if selected
+      setIsSelected(true);
+      setSectionOpen(true);
       setSiblingIsSelected(true);
-    } else if (depth > stateDispatchArray[focusedItem.id].depth) setSiblingIsSelected(false);
-  }, [depth, entry, focusedItem, renderChildren, setSiblingIsSelected, stateDispatchArray]);
+    } else {
+      // if not selected
+      setIsSelected(false);
+      if (entry.depth > focusedItem.depth) {
+        setSiblingIsSelected(false);
+      }
+      if (entry.depth >= focusedItem.depth) {
+        setSectionOpen(false);
+      }
+    }
+  }, [entry, focusedItem, renderChildren, setSiblingIsSelected]);
 
   useEffect(() => {
     if (!renderChildren) return;
@@ -64,52 +62,30 @@ const ExpandingLayer = ({
 
     api.start({
       to: {
-        height: sectionOpen ? `${(listHeight - depth) * 1.5}em` : "0",
+        height: sectionOpen ? `${(listHeight - entry.depth) * 1.5}em` : "0",
       },
       config: {
         ...springsConfig,
         clamp: !sectionOpen,
       },
     });
-  }, [api, depth, entry, listHeight, renderChildren, sectionOpen]);
+  }, [api, entry, listHeight, renderChildren, sectionOpen]);
 
   if (!renderChildren) return; // Restrict rendering to avoid max render
 
   const handleExpand = () => {
-    // console.log("LayerState: ", stateDispatchArray);
-
     setHasBeenSelected(true);
 
     if (sectionOpen) {
       // Layer closing:
       setFocusedItem(parentEntry);
       setListHeight(parentsListHeight);
-      stateDispatchArray[parentId].select(true);
-      stateDispatchArray[entry.id].select(false);
-      stateDispatchArray[entry.id].expand(false);
-      // Object.keys(stateDispatchArray).forEach((id) => {
-      //   if (stateDispatchArray[id].depth >= depth) stateDispatchArray[id].setSiblingIsSelected(false);
-      // });
     } else {
       // Layer opening:
       setFocusedItem(entry);
-      setListHeight(entry.children.length + depth);
-      stateDispatchArray[entry.id].expand(true);
-      stateDispatchArray[entry.id].select(true);
-      // setSiblingIsSelected(true);
-
-      // Deselect and close other layers:
-      Object.keys(stateDispatchArray).forEach((id) => {
-        // if (stateDispatchArray[id].depth >= depth + 1) stateDispatchArray[id].setSiblingIsSelected(false);
-        if (id !== entry.id) {
-          stateDispatchArray[id].select(false);
-          if (stateDispatchArray[id].depth >= depth) stateDispatchArray[id].expand(false);
-        }
-      });
+      setListHeight(entry.children.length + entry.depth);
     }
   };
-
-  if (renderChildren) console.log("Selected: ", { focusedItem, stateDispatch: stateDispatchArray[focusedItem?.id] });
 
   const hideItem = !root && siblingIsSelected && parentIsOpen && !sectionOpen;
 
@@ -139,22 +115,18 @@ const ExpandingLayer = ({
             {entry.children.map((nextEntry) => (
               <ExpandingLayer
                 key={nextEntry.id}
-                entry={nextEntry}
+                entry={{ ...nextEntry, depth: entry.depth + 1 }}
                 onSelect={onSelect}
-                stateDispatchArray={stateDispatchArray}
-                setStateDispatchArray={setStateDispatchArray}
-                parentId={entry.id}
                 parentIsOpen={sectionOpen}
                 siblingIsSelected={selectionMade}
                 setSiblingIsSelected={setSelectionMade}
                 renderChildren={hasBeenSelected}
                 listHeight={listHeight}
                 setListHeight={setListHeight}
-                parentsListHeight={entry.children.length + depth}
+                parentsListHeight={entry.children.length + entry.depth}
                 focusedItem={focusedItem}
                 setFocusedItem={setFocusedItem}
                 parentEntry={entry}
-                depth={depth + 1}
               />
             ))}
           </animated.div>
@@ -165,22 +137,17 @@ const ExpandingLayer = ({
 };
 
 const Accordion = ({ directories, onSelect }) => {
-  const [stateDispatchArray, setStateDispatchArray] = useState([]);
   const [selectionMade, setSelectionMade] = useState(false);
   const [listHeight, setListHeight] = useState(0);
   const [focusedItem, setFocusedItem] = useState(null);
-  // console.log("DIRECTORIES: ", directories);
 
   return (
     <div className={styles.root}>
-      {directories.map((entry) => (
+      {directories.children.map((entry) => (
         <ExpandingLayer
           key={entry.id}
-          entry={entry}
+          entry={{ ...entry, depth: 0 }}
           onSelect={onSelect}
-          stateDispatchArray={stateDispatchArray}
-          setStateDispatchArray={setStateDispatchArray}
-          parentId={entry.id}
           parentIsOpen
           siblingIsSelected={selectionMade}
           setSiblingIsSelected={setSelectionMade}
@@ -190,7 +157,7 @@ const Accordion = ({ directories, onSelect }) => {
           parentsListHeight={entry.children.length}
           focusedItem={focusedItem}
           setFocusedItem={setFocusedItem}
-          depth={0}
+          parentEntry={{ ...directories, depth: 0 }}
           root
         />
       ))}

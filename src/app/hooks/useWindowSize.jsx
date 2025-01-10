e6d65493f4c8d298134fa08bec56ffd7b3bfcbc0
Dan <dan@waxworlds.org>
2025-01-10 15:35:16 +0000

    Extended functionality of useWindowSize hook

--- src/app/hooks/useWindowSize.jsx
+++ src/app/hooks/useWindowSize.jsx
@@ -1,12 +1,28 @@
 import { useState, useEffect } from "react";
+import breakpoints from "@/style/breakpoints.json";
 
-const useWindowSize = () => {
+const useWindowSize = (customValue = null) => {
   const [windowSize, setWindowSize] = useState({
     height: undefined,
     width: undefined,
   });
 
-  const handleResize = () => setWindowSize({ height: window.innerHeight, width: window.innerWidth });
+  const handleResize = () =>
+    setWindowSize({
+      height: window.innerHeight,
+      width: window.innerWidth,
+      aboveSm: window.innerWidth >= breakpoints["screen-sm"],
+      belowSm: window.innerWidth < breakpoints["screen-sm"],
+      aboveMd: window.innerWidth >= breakpoints["screen-md"],
+      belowMd: window.innerWidth < breakpoints["screen-md"],
+      aboveLg: window.innerWidth >= breakpoints["screen-lg"],
+      belowLg: window.innerWidth < breakpoints["screen-lg"],
+      aboveXl: window.innerWidth >= breakpoints["screen-xl"],
+      belowXl: window.innerWidth < breakpoints["screen-xl"],
+      aboveXxl: window.innerWidth >= breakpoints["screen-xxl"],
+      belowXxl: window.innerWidth < breakpoints["screen-xxl"],
+      ...(customValue && { aboveCust: window.innerWidth >= customValue, belowCust: window.innerWidth < customValue }),
+    });
 
   useEffect(() => {
     window.addEventListener("resize", handleResize);

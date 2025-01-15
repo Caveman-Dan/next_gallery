b56ff52c9c179b3c6861a3694a965201e4532651
Dan <dan@waxworlds.org>
2025-01-15 17:01:33 +0000

    Refactored windowSize hook

--- src/app/hooks/useWindowSize.jsx
+++ src/app/hooks/useWindowSize.jsx
@@ -1,30 +1,26 @@
 import { useState, useEffect } from "react";
 import breakpoints from "@/style/breakpoints.json";
 
-const useWindowSize = (customValue = null) => {
-  const [windowSize, setWindowSize] = useState({
-    height: undefined,
-    width: undefined,
-  });
-
-  const handleResize = () =>
-    setWindowSize({
-      height: window.innerHeight,
-      width: window.innerWidth,
-      aboveSm: window.innerWidth >= breakpoints["screen-sm"],
-      belowSm: window.innerWidth < breakpoints["screen-sm"],
-      aboveMd: window.innerWidth >= breakpoints["screen-md"],
-      belowMd: window.innerWidth < breakpoints["screen-md"],
-      aboveLg: window.innerWidth >= breakpoints["screen-lg"],
-      belowLg: window.innerWidth < breakpoints["screen-lg"],
-      aboveXl: window.innerWidth >= breakpoints["screen-xl"],
-      belowXl: window.innerWidth < breakpoints["screen-xl"],
-      aboveXxl: window.innerWidth >= breakpoints["screen-xxl"],
-      belowXxl: window.innerWidth < breakpoints["screen-xxl"],
-      ...(customValue && { aboveCust: window.innerWidth >= customValue, belowCust: window.innerWidth < customValue }),
-    });
+const useWindowSize = () => {
+  const [windowSize, setWindowSize] = useState({});
 
   useEffect(() => {
+    const handleResize = () =>
+      setWindowSize({
+        height: window.innerHeight,
+        width: window.innerWidth,
+        aboveSm: window.innerWidth >= breakpoints["screen-sm"],
+        belowSm: window.innerWidth < breakpoints["screen-sm"],
+        aboveMd: window.innerWidth >= breakpoints["screen-md"],
+        belowMd: window.innerWidth < breakpoints["screen-md"],
+        aboveLg: window.innerWidth >= breakpoints["screen-lg"],
+        belowLg: window.innerWidth < breakpoints["screen-lg"],
+        aboveXl: window.innerWidth >= breakpoints["screen-xl"],
+        belowXl: window.innerWidth < breakpoints["screen-xl"],
+        aboveXxl: window.innerWidth >= breakpoints["screen-xxl"],
+        belowXxl: window.innerWidth < breakpoints["screen-xxl"],
+      });
+
     window.addEventListener("resize", handleResize);
     handleResize();
 

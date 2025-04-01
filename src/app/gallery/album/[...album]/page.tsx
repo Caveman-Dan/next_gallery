c2eb8aea7abc5bf2eb3c2e88cd1ca89223a1e690
Dan <dan@waxworlds.org>
2025-04-01 11:14:54 +0100

    WIP - useElementSize is implemented and working

--- src/app/gallery/album/[...album]/page.tsx
+++ src/app/gallery/album/[...album]/page.tsx
@@ -1,55 +1,88 @@
-"use server";
+"use client";
 
-import React from "react";
+import React, { useState, useEffect, useLayoutEffect, useRef } from "react";
+import { useParams } from "next/navigation";
 import uniqid from "uniqid";
 
 import { getImages } from "@/lib/actions";
-import ImageThumb from "@/ui/gallery/ImageThumb/ImageThumb";
-// import Image from "@/ui/components/Image/Image";
+import ImageThumb from "@/ui/Album/ImageThumb/ImageThumb";
+import useElementSize from "@/hooks/useElementSize";
 
 import type { NextPage } from "next";
 import { ImageDetails, ApiErrorResponse } from "@/definitions/definitions";
 
 import styles from "./page.module.scss";
 
-const Page: NextPage<{
-  params: Promise<{
-    album: [];
-  }>;
-}> = async ({ params }: { params: Promise<{ album: string[] }> }) => {
-  const albumPath = decodeURIComponent((await params).album.join("/"));
-  const images = await getImages(albumPath);
+const ImageSequencer = ({
+  images,
+  albumPath,
+  containerWidth,
+}: {
+  images: ImageDetails[];
+  albumPath: string;
+  containerWidth: number | undefined;
+}) => {
+  return (
+    <>
+      <p>{containerWidth}</p>
+      {(images as ImageDetails[])?.map((item: ImageDetails) => {
+        const imageUrl = new URL(
+          `${process.env.NEXT_PUBLIC_API_GET_IMAGE}/${albumPath}/${item.fileName}`,
+          process.env.NEXT_PUBLIC_API
+        );
 
-  if ((images as ApiErrorResponse)?.error)
-    throw Error((images as ApiErrorResponse)?.message, { cause: { status: (images as ApiErrorResponse)?.status } });
+        return (
+          <>
+            <ImageThumb
+              key={item.fileName}
+              src={imageUrl.href}
+              srcWidth={item.details.width}
+              srcHeight={item.details.height}
+              thumbWidth={450}
+              thumbHeight={225}
+              alt={`image of ${item.fileName}`}
+              placeholder="blur"
+              blurDataURL={item.placeholder.blurData}
+              albumPath={albumPath}
+              fileName={item.fileName}
+            />
+          </>
+        );
+      })}{" "}
+    </>
+  );
+};
+
+const Page: NextPage = () => {
+  // return null;
+  const containerRef = useRef<HTMLDivElement>(null);
+  const albumPath = decodeURIComponent(useParams<{ album: string[] }>().album.join("/"));
+  const [images, setImages] = useState<ImageDetails[] | []>([]);
+
+  const { clientWidth: containerWidth } = useElementSize(containerRef?.current);
+
+  useLayoutEffect(() => {
+    if (images.length && containerWidth) return;
+
+    getImages(albumPath).then((response) => {
+      if ((response as ApiErrorResponse)?.error) {
+        throw Error((response as ApiErrorResponse)?.message, {
+          cause: { status: (response as ApiErrorResponse)?.status },
+        });
+      } else {
+        setImages(response as ImageDetails[]);
+      }
+    });
+  }, [albumPath, containerWidth, images.length]);
 
   return (
     <>
-      <h1>{albumPath.split("/").reverse()[0]}</h1>
-      <p>{albumPath}</p>
-      <div className={styles.imagesContainer}>
-        {(images as ImageDetails[])?.map(async (item: ImageDetails) => {
-          const imageUrl = new URL(
-            `${process.env.NEXT_PUBLIC_API_GET_IMAGE}/${albumPath}/${item.fileName}`,
-            process.env.NEXT_PUBLIC_API
-          );
-
-          return (
-            <>
-              <ImageThumb
-                key={uniqid()}
-                src={imageUrl.href}
-                width={item.details.width}
-                height={item.details.height}
-                alt={`image of ${item.fileName}`}
-                placeholder="blur"
-                blurDataURL={item.placeholder.blurData}
-                albumPath={albumPath}
-                fileName={item.fileName}
-              />
-            </>
-          );
-        })}
+      <div className={styles.titleContainer}>
+        <h1>{albumPath.split("/").reverse()[0]}</h1>
+        <p>{albumPath}</p>
+      </div>
+      <div className={styles.imagesContainer} ref={containerRef}>
+        <ImageSequencer images={images} albumPath={albumPath} containerWidth={containerWidth} />
       </div>
     </>
   );

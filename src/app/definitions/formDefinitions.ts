a08cdd874162dfe4b19622f549d76aac83f3dfb2
Dan <dan@waxworlds.org>
2025-12-18 13:58:05 +0000

    Created TS definitions for form validation and the login form

---
+++ src/app/definitions/formDefinitions.ts
@@ -0,0 +1,39 @@
+export type InputState = {
+  value: string;
+  error: boolean;
+  message?: string;
+};
+
+export type FormState = {
+  [key: string]: InputState;
+};
+
+export type FormValues = { [key: string]: string };
+
+export type ValidatorOptions = {
+  error: string;
+  // extend as needed
+};
+
+export type ValidatorTest = (value: string, options: ValidatorOptions) => InputState;
+
+export type FieldTest = {
+  test: ValidatorTest;
+  options: ValidatorOptions;
+};
+
+export type FieldConfig = {
+  isRequired?: boolean; // optional, defaults to false if omitted
+  tests?: FieldTest[];
+};
+
+export type FieldsConfig = {
+  [key: string]: FieldConfig;
+};
+
+export type FormConfig = {
+  config: {
+    initialState: FormState;
+  };
+  fields: FieldsConfig;
+};

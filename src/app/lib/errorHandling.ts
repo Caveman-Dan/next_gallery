79d29f3a799b7840db3c6a451a28834218ceb8c8
Dan <dan@waxworlds.org>
2026-09-02 12:13:09 +0100

    Removed redundant import of chalk in errorHandling.ts

--- src/app/lib/errorHandling.ts
+++ src/app/lib/errorHandling.ts
@@ -1,15 +1,13 @@
-import chalk from "chalk";
-
 type errorProps = {
   message: string;
 };
 
 export const handleServerError = async (err: errorProps) => {
   console.log("THE ERROR: ", JSON.stringify(err));
-  console.error(chalk.redBright(err.message));
+  console.error(err.message);
 };
 
 export const handleClientError = (err: errorProps) => {
   console.log("THE ERROR: ", JSON.stringify(err));
-  console.error(chalk.redBright(err.message));
+  console.error(err.message);
 };
